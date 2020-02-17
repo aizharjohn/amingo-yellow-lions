c5001a8cbb9b98507488a5969db31f3bdcbf8b90
manishmenda <manish@myaconsulting.com>
2020-02-17 20:45:32 +0400

    changes to topic route to say TopicModel

--- routes/ShareTopic.js
+++ routes/ShareTopic.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const UserModel = require("../models/ShareTopic.js");
+const TopicModel = require("../models/ShareTopic.js");
 
 router.post(
-    "/topic", // http:/www.myapp.com/user/register
+    "/topic", 
     (req, res) => {
       const formdata = {
         topic: req.body.topic,
